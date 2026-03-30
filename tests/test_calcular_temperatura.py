a3af3499acb10b4d14a04f903445a357e08b75f6
Joel Avila <staymoon.edson@gmail.com>
2026-03-29 23:14:36 -0600

    Correcion tests

--- tests/test_calcular_temperatura.py
+++ tests/test_calcular_temperatura.py
@@ -1,19 +1,19 @@
 import pytest
 
-from src.calcular_temperatura import clasificar_temperatura 
+from src.calcular_temperatura import calcular_temperatura 
 
 @pytest.mark.parametrize("temp, esperado", [
-    (10, "Frío"),      # Partición: Frío 
+    (10, "Frio"),      # Partición: Frío 
     (20, "Templado"),  # Partición: Templado 
     (30, "Caliente")   # Partición: Caliente 
 ])
 def test_clasificar_temperatura_validas(temp, esperado):
-    assert clasificar_temperatura(temp) == esperado
+    assert calcular_temperatura(temp) == esperado
 
 def test_error_rango_temperatura():
     with pytest.raises(ValueError):
-        clasificar_temperatura(12000)
+        calcular_temperatura(12000)
 
 def test_error_tipo_temperatura():
     with pytest.raises(TypeError):
-        clasificar_temperatura("calorcito")
+        calcular_temperatura("calorcito")
